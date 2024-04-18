b48dfe189c1e8181c96224cc6aed5a11a55ad190
MarieG41 <mariegond8@gmail.com>
2024-04-18 14:48:27 +0200

    list task form page works

--- src/pages/FormInputs.jsx
+++ src/pages/FormInputs.jsx
@@ -1,75 +1,90 @@
-import React, { useContext, useState } from "react";
-import Button from 'react-bootstrap/Button';
-import Form from 'react-bootstrap/Form';
-import Container from 'react-bootstrap/Container';
-import TaskProvider from "../utils/context/TaskContext";
-import useFormValidation from "../utils/hooks/useFormValidation"
+import { useContext, useState } from "react";
+import { TaskContext } from "../utils/context/TaskContext.jsx";
+import { Form, Button, Container, Row } from "react-bootstrap";
+import useFormValidation from "../utils/hooks/useFormValidation.jsx";
 import { useNavigate } from "react-router-dom";
 
-export default function FormInputs() {
-  const { tasksProvide } = useContext(TaskProvider)
+const FormInput = () => {
+  const { addTask } = useContext(TaskContext);
   const [task, setTask] = useState({
     name: "",
-    priority: "low",
+    dueDate: "",
+    priority: "Basse",
     status: "En cours",
-  })
-  const { errors, validateTask } = useFormValidation
+  });
 
-  const navigate = useNavigate()
-  const handleSubmitForm = (event) => {
-    event.preventDefault()
-    if(!validateTask(task)) return
-    tasksProvide(task)
-    navigate("/")
-  }
-  const handleChangeForm = (event) => {
-    const { name, value } = event.target
+  const { errors, validateTask } = useFormValidation();
+
+  const navigate = useNavigate();
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (!validateTask(task)) return;
+
+    addTask(task);
+    navigate("/");
+  };
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
     setTask((prevTask) => ({
       ...prevTask,
       [name]: value,
-    }))    
-  }
-    return (
-        <Container>
-          <Form onSubmit={handleSubmitForm}>
-            <Form.Group className="mb-3" controlId="formBasicText">
+    }));
+  };
+
+  return (
+    <>
+      <Container>
+        <Row>
+          <Form onSubmit={handleSubmit}>
+            <Form.Group className="mb-3">
               <Form.Label>Nom de la tâche</Form.Label>
-              <Form.Control 
-                type="text" 
+              <Form.Control
+                type="text"
                 name="name"
                 value={task.name}
-                onChange={handleChangeForm}
+                onChange={handleChange}
                 isInvalid={!!errors.name}
-                required />
-                <Form.Control.Feedback type="invalid">
-                  {errors.name}
+              />
+              <Form.Control.Feedback type="invalid">
+                {errors.name}
               </Form.Control.Feedback>
             </Form.Group>
-            <Form.Group className="mb-3" controlId="formBasicDate">
-              <Form.Label>Date</Form.Label>
-              <Form.Control 
-                type="date" 
+            <Form.Group className="mb-3">
+              <Form.Label>Date due</Form.Label>
+              <Form.Control
+                type="text"
                 name="dueDate"
-                onChange={handleChangeForm}
-                required />
-                <Form.Control.Feedback type="invalid">
-                  {errors.name}
+                value={task.dueDate}
+                onChange={handleChange}
+                placeholder="jj/mm/aaaa"
+                isInvalid={!!errors.dueDate}
+              />
+              <Form.Control.Feedback type="invalid">
+                {errors.dueDate}
               </Form.Control.Feedback>
             </Form.Group>
-            <Form.Select 
-              name="priority"
-              value={task.priority}
-              onChange={handleChangeForm}
-            >
-              <option>Priorité</option>
-              <option value="low">Basse</option>
-              <option value="moderate">Moyenne</option>
-              <option value="high">Elevée</option>
-            </Form.Select>
-            <Button type="submit" variant="primary">
+            <Form.Group className="mb-3">
+              <Form.Label>Priorité</Form.Label>
+              <Form.Select
+                name="priority"
+                value={task.priority}
+                onChange={handleChange}
+              >
+                <option value="Basse">Basse</option>
+                <option value="Moyenne">Moyenne</option>
+                <option value="Elevée">Elevée</option>
+              </Form.Select>
+            </Form.Group>
+            <Button variant="primary" type="submit">
               Ajouter la tâche
             </Button>
           </Form>
-        </Container>
-    );
-}
+        </Row>
+      </Container>
+    </>
+  );
+};
+
+export default FormInput;
--- src/utils/hooks/useFormValidation.jsx
+++ src/utils/hooks/useFormValidation.jsx
@@ -1,17 +1,27 @@
+// useFormValidation.js
 import { useState } from "react";
+import { validateDate } from "../helpers/validateDate.jsx";
 
 const useFormValidation = () => {
-    const [ errors, setErrors ] = useState({})
-    const validateTask = (task) => {
-        const newErrors = {}
-        if(!task.name || task.name.length < 8) {
-            newErrors.name = "Le nom de la tâche doit contenir au moins 8 caractères."
-        }
+  const [errors, setErrors] = useState({});
 
-        setErrors(newErrors)
-        return Object.keys(newErrors).length === 0
+  const validateTask = (task) => {
+    const newErrors = {};
+    if (!task.name || task.name.length < 8) {
+      newErrors.name =
+        "Le nom de la tâche doit contenir au moins 8 caractères.";
     }
-    return { errors, validateTask }
-}  
+    if (!validateDate(task.dueDate)) {
+      newErrors.dueDate =
+        "La date due doit être au format dd/mm/YYYY et ne doit pas être antérieure à la date du jour.";
+    }
+    
+    setErrors(newErrors);
 
-export default useFormValidation
+    return Object.keys(newErrors).length === 0;
+  };
+
+  return { errors, validateTask };
+};
+
+export default useFormValidation;
